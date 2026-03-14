3003e2d7f1e2b7cf81aa1be7c426c449c8f8e86c
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2026-03-14 14:55:15 -0700

    new startup

--- startup.sh
+++ startup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y git python3 python3-pip
+sudo apt-get install -y git python3 python3-flask python3-requests
 
 cd /home
 sudo git clone https://github.com/aaron-dm-mcdonald/theo-website-gcp.git
 cd theo-website-gcp
-
-sudo pip3 install flask requests
 
 nohup python3 app.py > output.log 2>&1 &
